bba239cef440074b64614ad4126f0d4ca9145595
Josef Jetmar <josef.jetmar@unicorn.cz>
2019-01-24 21:14:12 +0100

    squash

--- test/my-test.js
+++ test/my-test.js
@@ -6,19 +6,20 @@ const RandExp = require('../lib/randexp');
 
 describe('Initialize RandExp with global()', () => {
     describe('Create a regular expression', () => {
-        var regexp = /[a-c]{3,5}gg(ddd[4-9]{0,3})r{5,}/;
+        var regexp = /^[a-c]{3,5}gg(ddd[4-9]{0,3})r{5,}$/;
         var randexp = new RandExp(regexp);
 
         it('Run a tst for my debug', () => {
             for (var i = 0 ; i < 1000 ; i++) {
                 var valid = randexp.genValid();
                 if (!regexp.test(valid)) {
-                    console.log("not valid: " + valid);
+                    console.log("#" + i + " - not valid: " + valid);
                     assert.equal(regexp.test(valid), true);
                 }
+
                 var invalid = randexp.genInvalid();
                 if (regexp.test(invalid)) {
-                    console.log("not invalid: " + invalid);
+                    console.log("#" + i + " not invalid: " + invalid);
                     assert.equal(regexp.test(invalid), false);
                 }
             }
